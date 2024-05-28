b7988b9619870d6f977f47b2fb7540e9e742c86a
andrey <me@messus.ru>
2024-05-28 20:09:09 +0700

    add my_timeit.py

---
+++ Chapter_one/my_timeit.py
@@ -0,0 +1,10 @@
+from timeit import timeit
+
+import dis
+
+a_list = list(range(1_000_000))
+
+print(timeit("list[0]", "from __main__ import a_list"))
+print(timeit("list[500000]", "from __main__ import a_list"))
+print(timeit("list[-1]", "from __main__ import a_list"))
+
