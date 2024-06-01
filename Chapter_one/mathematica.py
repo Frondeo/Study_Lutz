6e48eb0505869080353fe84214de8a09a6c47df2
andrey <me@messus.ru>
2024-06-01 20:42:17 +0700

    add mathematica.py

---
+++ Chapter_one/mathematica.py
@@ -0,0 +1,20 @@
+class Mathematica:
+    def __init__(self, name, perimeter, area):
+        self.name = name
+        self.perimeter = perimeter
+        self.area = area
+
+    def calculate_area(self):
+        return self.perimeter ** 2
+    def __str__(self):
+        return f'{self.__class__.__name__} {self.name} is perimeter {self.perimeter} and area {self.calculate_area}'
+
+class Figure(Mathematica):
+    def __init__(self, name, perimeter):
+       super().__init__(name, perimeter)
+
+if __name__ == '__main__':
+    triangle = Figure('triangle', 30)
+    print(triangle)
+
+
